5191da0071da928efc59c4d0867a4d6a81fae710
Nurislamxt <snakeryzon@gmail.com>
2026-01-03 20:51:09 +0500

    fix: correct paths in routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,9 @@
 import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-  // Главная страница
-  index("app/page.jsx"),
+  // Убираем "app/", так как мы уже находимся внутри этой папки согласно конфигу
+  index("page.jsx"),
 
-  // Страница 404 (обработка ошибок)
-  route("*", "app/__create/not-found.tsx"),
+  // Страница 404
+  route("*", "__create/not-found.tsx"),
 ] satisfies RouteConfig;
